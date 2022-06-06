454aef0bb3ed8082b7ab89876e18479ab44a3016
K-Rdlbrgr <34638@novasbe.pt>
2022-06-06 16:39:35 +0200

    Remove a hardcoded Django secret key

--- apartmany/settings.py
+++ apartmany/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^sz60q1*&mc2$p^eu!buglfmwd21w_kc!+z-(pm)4u_k4%4tfd'
+# SECRET_KEY = 'django-insecure-^sz60q1*&mc2$p^eu!buglfmwd21w_kc!+z-(pm)4u_k4%4tfd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
