1c87fcb0dc49c7dc02507f093bdd5be44b498beb
K-Rdlbrgr <34638@novasbe.pt>
2022-06-06 22:39:27 +0200

    update URLs

--- apartmany/urls.py
+++ apartmany/urls.py
@@ -13,8 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
@@ -26,3 +28,5 @@ urlpatterns += i18n_patterns(
     path('', include('generic.urls')),
     prefix_default_language=False
 )
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
